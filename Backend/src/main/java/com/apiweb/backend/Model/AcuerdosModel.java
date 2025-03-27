9ef8c8162ca7c8855970ca0900669502304d7102
juan vila <jvilamarin31@gmail.com>
2025-03-27 14:43:22 -0500

    hice acuerdos

---
+++ Backend/src/main/java/com/apiweb/backend/Model/AcuerdosModel.java
@@ -0,0 +1,30 @@
+package com.apiweb.backend.Model;
+
+import java.time.Instant;
+
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document ("ACUERDOS")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class AcuerdosModel {
+    @Id
+    private Object id;
+    private Object idInmueble;
+    private Instant  Inicio;
+    private Instant  fechaFin;
+    private String razonCancelacion;
+    private String estado;
+    private String archivoContrato;
+    private ArrendatarioAcuerdoModel arrendatario;// Este atributo hace ref a el documento embebido
+
+ 
+} 
---
+++ Backend/src/main/java/com/apiweb/backend/Model/ArrendatarioAcuerdoModel.java
@@ -0,0 +1,16 @@
+package com.apiweb.backend.Model;
+
+import org.springframework.data.annotation.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ArrendatarioAcuerdoModel {
+    @Id
+    private Object idUsuario;
+    private String nombre;
+}
