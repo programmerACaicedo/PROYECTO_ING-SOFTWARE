94437aa0198aed5b70577e1f7606e3086e310162
juan vila <jvilamarin31@gmail.com>
2025-03-27 18:21:19 -0500

    Ya quedo Acuerdos

---
+++ Backend/src/main/java/com/apiweb/backend/Controller/AcuerdosController.java
@@ -0,0 +1,23 @@
+package com.apiweb.backend.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.apiweb.backend.Model.AcuerdosModel;
+import com.apiweb.backend.Service.IAcuerdosService;
+
+@RestController
+@RequestMapping("/api/acuerdos")
+public class AcuerdosController {
+    @Autowired IAcuerdosService acuerdosService;
+    @PostMapping("/insertar")
+    public ResponseEntity<AcuerdosModel> crearAcuerdo (@RequestBody AcuerdosModel acuerdo) {
+        return new ResponseEntity<AcuerdosModel>(acuerdosService.guardarAcuerdo(acuerdo),HttpStatus.CREATED);
+    }
+    
+}
