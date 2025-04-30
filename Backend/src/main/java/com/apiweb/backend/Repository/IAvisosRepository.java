79315b0ece5dbca79de90b8b17ab139031ab8be3
Juanjo <jjsolarte14@gmail.com>
2025-04-30 01:19:45 -0500

    corrección error

--- Backend/src/main/java/com/apiweb/backend/Repository/IAvisosRepository.java
+++ Backend/src/main/java/com/apiweb/backend/Repository/IAvisosRepository.java
@@ -1,5 +1,6 @@
 package com.apiweb.backend.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.bson.types.ObjectId;
@@ -10,4 +11,7 @@ import com.apiweb.backend.Model.UbicacionAviso;
 
 public interface IAvisosRepository extends MongoRepository<AvisosModel, ObjectId> {
     Optional<AvisosModel> findByUbicacion(UbicacionAviso ubicacion);
+    void deleteByPropietarioId_Usuario_id(ObjectId usuarioId);
+    List<AvisosModel> findByPropietarioIdUsuarioId(ObjectId propietarioId);
+    List<AvisosModel> findByReporteIsNotNull();
 }
