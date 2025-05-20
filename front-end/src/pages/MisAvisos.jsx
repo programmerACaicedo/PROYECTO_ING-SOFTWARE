03f54e78489dbe45af737a4d85f57e4571a9d3d7
Juanjo <jjsolarte14@gmail.com>
2025-05-20 01:15:41 -0500

    css filtro misAvisos

--- front-end/src/pages/MisAvisos.jsx
+++ front-end/src/pages/MisAvisos.jsx
@@ -196,12 +196,7 @@ const MisAvisos = () => {
                 )}
                 <p>Precio: {pub.precio_mensual ? `$${pub.precio_mensual.toLocaleString()}` : "No especificado"}</p>
                 <p>Descripción: {pub.descripcion}</p>
-                {/* Mostrar "Estado Reporte" si hay un reporte, de lo contrario mostrar "Estado" */}
-                {!pub.reporte || pub.reporte.estadoReporte === "AvisoActualizado" ? (
-                  <p>Estado: {pub.estado}</p>
-                ) : (
-                  <p>Estado Reporte: {pub.reporte.estadoReporte}</p>
-                )}
+                <p>Estado: {pub.estado}</p>
               </div>
             ))}
           </div>
