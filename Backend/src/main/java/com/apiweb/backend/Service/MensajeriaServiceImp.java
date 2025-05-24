90a64449787ad43752ae3195bde1f9dcad3513df
juan vila <jvilamarin31@gmail.com>
2025-05-24 12:36:07 -0500

    mejorar manejo de excepciones en MensajeriaServiceImp y ajustar validaciones de usuario

--- Backend/src/main/java/com/apiweb/backend/Service/MensajeriaServiceImp.java
+++ Backend/src/main/java/com/apiweb/backend/Service/MensajeriaServiceImp.java
@@ -11,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.apiweb.backend.Exception.InvalidMensajeriaConfigurationException;
 import com.apiweb.backend.Exception.InvalidUserRoleException;
 import com.apiweb.backend.Exception.ResourceNotFoundException;
+import com.apiweb.backend.Exception.UserNotFoundException;
 import com.apiweb.backend.Model.AvisosModel;
 import com.apiweb.backend.Model.MensajeriaModel;
 import com.apiweb.backend.Model.MensajesMensajeria;
@@ -42,12 +43,17 @@ public class MensajeriaServiceImp implements IMensajeriaService{
         AvisosModel aviso = avisoExiste.get();
         Optional<UsuariosModel> usuarioExiste = UsuarioRepository.findById(chat.getIdInteresado());
         if (!usuarioExiste.isPresent()) {
-            throw new ResourceNotFoundException("El usuario con id: "+ chat.getIdInteresado()+" no existe.");
+            throw new UserNotFoundException("El usuario con id: "+ chat.getIdInteresado()+" no existe.");
         }
         UsuariosModel interesado = usuarioExiste.get();
-        if (interesado.getTipo() != TipoUsuario.interesado) {
-            throw new InvalidUserRoleException("Solo un usuario el tipo 'interesado' puede crear un chat. ");
+
+        if (interesado.getId()==aviso.getPropietarioId().getUsuarioId()) {
+            throw new InvalidUserRoleException("Un usuario no se puede interesar por un aviso que es de supropiedad. ");
         }
+        if (interesado.getTipo() == TipoUsuario.administrador) {
+            throw new InvalidUserRoleException("Un administrador no se puede interesar por un aviso. ");
+        }
+
         Optional<MensajeriaModel> mensajeExiste = MensajeriaRepository.findByIdInteresadoAndIdAviso(chat.getIdInteresado(),chat.getIdAviso());
         if (mensajeExiste.isPresent()){
             throw new InvalidMensajeriaConfigurationException("El interesado con id: " + chat.getIdInteresado() + " ya tiene un chat creado con ese aviso");
@@ -72,26 +78,31 @@ public class MensajeriaServiceImp implements IMensajeriaService{
         AvisosModel aviso = avisoExiste.get();
 
         Optional<UsuariosModel> usuarioExiste = UsuarioRepository.findById(mensajes.getIdRemitente());
+        if (!usuarioExiste.isPresent()) {
+            throw new UserNotFoundException("El id: "+ mensajes.getIdRemitente() + " no pertenece a ningun usuario existente. ");
+        }
         UsuariosModel usuario = usuarioExiste.get();
+
+        if (usuario.getId() == chat.idInteresado || usuario.getId() == aviso.getPropietarioId().getUsuarioId()) {
+            throw new InvalidUserRoleException("El usuario remitente debe ser el mismo usuario que creo el chat o el propietario del aviso. ");
+        }
         
 
         if (chat.getMensajes() == null || chat.getMensajes().isEmpty()) {
-            if (usuario.getTipo() != TipoUsuario.propietario) {
+            if (usuario.getId() != aviso.getPropietarioId().getUsuarioId()) {
                 throw new InvalidUserRoleException("El propietario debe responder el mensaje del interesado. ");
             }
-            if (!usuario.getId().equals(aviso.getPropietarioId().getUsuarioId())){
-                throw new InvalidUserRoleException("El propietario ingresado no es el mismo propietario del aviso. ");
-            }
+
         } else {
             MensajesMensajeria ultimoMensaje = chat.getMensajes().get(chat.getMensajes().size() - 1);
 
             if (ultimoMensaje.getIdRemitente().equals(chat.getIdInteresado())) {
-                if (usuario.getTipo() != TipoUsuario.propietario) {
+                if (usuario.getId() != aviso.getPropietarioId().getUsuarioId()) {
                     throw new InvalidUserRoleException("El propietario debe responder al mensaje del interesado.");
                 }
             } else {
                 // Si el último mensaje fue enviado por el propietario, la respuesta debe venir del interesado.
-                if (usuario.getTipo() != TipoUsuario.interesado) {
+                if (usuario.getId() != chat.idInteresado) {
                     throw new InvalidUserRoleException("El interesado debe responder al mensaje del propietario.");
                 }
             }
